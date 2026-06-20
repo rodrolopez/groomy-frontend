86811f2d2159ba0a9655cffebfbc0b5346259e4b
Rodrigo_Lopez <rodrilopez01@gmail.com>
2026-06-20 00:22:37 -0300

    fix: hardcodeo del tunel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 // Ponemos la URL fija para asegurar el tiro en producción. Si estás local usás localhost.
-const API_URL = "https://groomy-back.loca.lt/api";
+const API_URL = "https://foolish-newt-83.loca.lt";
 
 const nextConfig: NextConfig = {
   output: "standalone",
